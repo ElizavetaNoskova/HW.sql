14ea80d5a801b46e2714446b5d31ad3a98ae4969
Елизавета Носкова <wliza68@gmail.com>
2023-04-16 15:44:22 +0300

    3 ЗАДАНИЕ

--- HW.sql
+++ HW.sql
@@ -62,4 +62,39 @@ WHERE age = (SELECT MIN(age) FROM employee);
 select first_name AS имя, MAX(age) AS МАКСИМАЛЬНЫЙ_ВОЗРАСТ
 FROM employee
 GROUP BY first_name
-HAVING COUNT(first_name)>1;
+HAVING COUNT(first_name) > 1;
+CREATE TABLE city;
+( city_id BIGSERIAL NOT NULL PRIMARY KEY,
+    city_name VARCHAR (50) NOT NULL );
+ALTER TABLE employee
+    add column city_id INT;
+--Назначьте эту колонку внешним ключом. Свяжите таблицу employee с таблицей city
+alter table employee
+add foreign key (city_id) references city(city_id);
+insert into city(city_name)
+values ('Tver'),
+        ('Moskva'),
+        ('Omsk'),
+        ('S.T.Petersburg');
+UPDATE employee SET city_id = 1 WHERE id = 2;
+UPDATE employee SET city_id = 2 WHERE id = 5;
+UPDATE employee SET city_id = 3 WHERE id = 3;
+UPDATE employee SET city_id = 4 WHERE id = 4;
+UPDATE employee SET city_id = 5 WHERE id = 6;
+select employee.first_name, employee.last_name, city.city_name from employee
+join city on employee.city_id = city.city_id;
+
+select city.city_name, employee.first_name, employee.last_name from city
+left join employee on employee.city_id = city.city_id;
+select employee.first_name, city.city_name
+from employee
+full join city on employee.city_id = city.city_id;
+
+select employee.first_name, city.city_name
+from employee
+left join city on employee.city_id = city.city_id;
+
+select city.city_name
+from employee
+right join city on employee.city_id = city.city_id
+where employee.first_name is null;
